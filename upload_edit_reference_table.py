8847a4cbd2f2947ce82228645c87b8a6d7e565d3
Miroslav Šollar <52739918+mirasollar@users.noreply.github.com>
2026-01-09 15:26:32 +0100

    Fix table ID reference in column settings retrieval

--- upload_edit_reference_table.py
+++ upload_edit_reference_table.py
@@ -701,11 +701,11 @@ elif st.session_state['upload-tables']:
                     table_id = selected_bucket + '.' + table_name
                     st.session_state["uploaded_table_id"] = table_id
                     # column_setting = get_setting(token, selected_bucket, table_id)[0]
-                    column_setting = get_column_settings(kbc_token, settings_table_id, selected_row['table_id'], f"settings_{get_table_name_suffix()}")[0]
+                    column_setting = get_column_settings(kbc_token, settings_table_id, table_id, f"settings_{get_table_name_suffix()}")[0]
                     format_setting = split_dict(column_setting, 2)
                     null_cells_setting = split_dict(column_setting, 1)
                     # case_sensitive_setting = get_setting(token, selected_bucket, table_id)[3]
-                    case_sensitive_setting = get_column_settings(kbc_token, settings_table_id, selected_row['table_id'], f"settings_{get_table_name_suffix()}")[1]
+                    case_sensitive_setting = get_column_settings(kbc_token, settings_table_id, table_id, f"settings_{get_table_name_suffix()}")[1]
                     primary_key_setting = get_setting(token, selected_bucket, table_id)[1]
                     date_setting = date_setting(column_setting)
                     if Path(uploaded_file.name).suffix == '.csv':

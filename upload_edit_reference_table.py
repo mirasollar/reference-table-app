b349107126e4a8d1d73713b46bc756d4e7be4957
Miroslav Šollar <52739918+mirasollar@users.noreply.github.com>
2026-01-12 16:29:12 +0100

    Clear selected table after successful save
    Reset selected table state after saving data.

--- upload_edit_reference_table.py
+++ upload_edit_reference_table.py
@@ -665,6 +665,8 @@ elif st.session_state['selected-table'] is not None:
                     st.session_state["edited_data"] = modifying_nas(edited_data)
                 st.success("Metadata validated successfully!", icon = "🎉")
                 st.session_state["save_requested"] = True
+                # pokus - refresh po save data
+                st.session_state['selected-table'] = None
                 st.rerun()
 
     # Pokud bylo kliknuto na "Save" a vyžaduje se přihlášení, ale uživatel není přihlášený, zobrazí se login

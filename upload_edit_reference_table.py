fada618abd9552e091568636269fa9781fe16a88
Miroslav Šollar <52739918+mirasollar@users.noreply.github.com>
2026-03-13 15:00:10 +0100

    Update upload_edit_reference_table.py

--- upload_edit_reference_table.py
+++ upload_edit_reference_table.py
@@ -425,8 +425,9 @@ def get_now_utc():
     return now_utc.strftime('%Y-%m-%d, %H:%M:%S')
 
 def get_table_name_suffix():
-    headers = st.context.headers
-    return re.sub('-', '_', headers['Host'].split('.')[0])
+    headers = st.context.headers 
+    host = re.sub('-', '_', headers['Host'].split('.')[0])
+    return re.sub('app_', '', host)
 
 settings_table_id = f"in.c-reference_tables_metadata.settings_{get_table_name_suffix()}"
 
